496f5ffb28dbc51712477273b9d363590e7832b0
Charramma <huang.zyn@qq.com>
2024-02-02 23:07:42 +0800

    .

--- frontend/src/view/test/Modal.vue
+++ frontend/src/view/test/Modal.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- <Modal v-model="visible" title="添加或修改表格数据"> -->
   <Modal :value="visible" title="添加或修改表格数据">
     <Form :model="modalForm" :label-width="80" ref="modalForm">
       <FormItem label="Name: " prop="name">
@@ -14,31 +13,32 @@
         <Button @click="resetForm('modalForm')" style="margin-left: 8px">重置</Button>
       </FormItem>
     </Form>
-    <div slot="footer">
-    </div>
+    <div slot="footer"></div> <!-- 此处仅为了禁用默认的底部按钮 -->
   </Modal>
 </template>
 
 <script>
   export default {
     name: 'ModalView',
+    // props的数据从父组件获取
     props: {
       visible: Boolean, // 对话框是否可见
       modalForm: Object // 对话框内联表单数据
     },
     methods: {
+      // 新增一条数据
       add() {
+        // 父组件触发自定义的add事件，通过$emit将this.modalForm的数据发送给父组件
         this.$emit('add', this.modalForm);
       },
+      // 修改一条数据
       change() {
+        // 父组件触发自定义的change事件，通过$emit将this.modalForm的数据发送给父组件
         this.$emit('change', this.modalForm);
       },
+      // 重置（清空）表单
       resetForm(name) {
-        // this.$emit('reset');
         this.$refs[name].resetFields();
-      },
-      updateVisible(value) {
-        this.$emit('update:visible', value);
       }
     },
   }
--- frontend/src/view/test/test.vue
+++ frontend/src/view/test/test.vue
@@ -9,6 +9,7 @@
 
 <script>
   import axios from 'axios';
+  // 引入子组件
   import TableVIew from './Table.vue';
   import ModalView from './Modal.vue';
 
@@ -47,16 +48,24 @@
                       this.editRow(params.index)
                     }
                   }
-                }, '编辑')
+                }, '编辑'),
+                h('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.delTableData(params.index)
+                    }
+                  }
+                }, '删除')
               ]);
             }
           }
         ],
-        // 表格数据，通过请求后端接口获取
-        tableData: [],
-
+        tableData: [],    // 表格数据，通过请求后端接口获取
         showModal: false, // 是否展示对话框
-
         // 对话框内联表单数据
         modalForm: {
           id: '',
@@ -66,12 +75,14 @@
       }
     },
     mounted() {
+      // 组件挂载时获取表格数据
       this.getTableData();
-
     },
     methods: {
+      // 获取表格数据
       getTableData() {
-        axios.get('http://192.168.5.98:5000/api/v1/test/temps')
+        // axios.get('http://192.168.5.98:5000/api/v1/test/temps')
+        axios.get('http://192.168.0.126:5000/api/v1/test/temps')
           .then(res => {
             this.tableData = res.data.data;
           })
@@ -79,8 +90,10 @@
             console.error(err);
           });
       },
+      // 新增表格数据
       addTableData(formData) {
-        axios.post('http://192.168.5.98:5000/api/v1/test/temps', formData)
+        // axios.post('http://192.168.5.98:5000/api/v1/test/temps', formData)
+        axios.post('http://192.168.0.126:5000/api/v1/test/temps', formData)
           .then(() => {
             this.showModal = false;
             this.getTableData();
@@ -89,6 +102,7 @@
             console.error(err);
           });
       },
+      // 修改表格数据
       editRow(index) {
         this.modalForm.id = this.tableData[index].id;
         this.modalForm.name = this.tableData[index].name;
@@ -98,7 +112,8 @@
       },
       changeTableData(formData) {
         const id = formData.id;
-        axios.put(`http://192.168.5.98:5000/api/v1/test/temp/${id}`, formData)
+        // axios.put(`http://192.168.5.98:5000/api/v1/test/temp/${id}`, formData)
+        axios.put(`http://192.168.0.126:5000/api/v1/test/temp/${id}`, formData)
           .then(() => {
             this.showModal = false;
             this.getTableData();
@@ -107,6 +122,20 @@
             console.error(err);
           });
       },
+      // 删除表格数据
+      delTableData(index) {
+        const id = this.tableData[index].id;
+        axios.delete(`http://192.168.0.126:5000/api/v1/test/temp/${id}`).then(() => {
+          this.showModal = false;
+          this.getTableData();
+        }).catch(err => {
+          console.error(err);
+        })
+      },
+      /* Modal的关闭逻辑是隐式的，子组件无法将showModal的值重置为false
+         导致页面刷新后第一次打开对话框后，showModal依然是true，无法再次打开对话框
+         在这个方法中先将对话框在逻辑上关闭，然后再打开，避免了上面的情况
+      */
       openModal() {
         this.showModal = false; // 先重置 showModal 的值为 false
         this.$nextTick(() => {
