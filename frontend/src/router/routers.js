fa4894bbbafcff6b4e5db6cf31a9f20395ea6e5a
Charramma <huang.zyn@qq.com>
2024-02-18 16:53:25 +0800

    .

--- frontend/src/router/routers.js
+++ frontend/src/router/routers.js
@@ -549,24 +549,24 @@ export default [{
 	// 		component: () => import('@/view/test/test.vue')
 	// 	}]
 	// },
-	{
-		path: '/message',
-		name: 'message',
-		component: Main,
-		meta: {
-			hideInBread: true,
-			hideInMenu: true
-		},
-		children: [{
-			path: 'message_page',
-			name: 'message_page',
-			meta: {
-				icon: 'md-notifications',
-				title: '消息中心'
-			},
-			component: () => import('@/view/single-page/message/index.vue')
-		}]
-	},
+	// {
+	// 	path: '/message',
+	// 	name: 'message',
+	// 	component: Main,
+	// 	meta: {
+	// 		hideInBread: true,
+	// 		hideInMenu: true
+	// 	},
+	// 	children: [{
+	// 		path: 'message_page',
+	// 		name: 'message_page',
+	// 		meta: {
+	// 			icon: 'md-notifications',
+	// 			title: '消息中心'
+	// 		},
+	// 		component: () => import('@/view/single-page/message/index.vue')
+	// 	}]
+	// },
 	{
 		path: '/argu',
 		name: 'argu',
